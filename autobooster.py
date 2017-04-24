bfe179351f0c0d9fe1467af542676a889f2932d7
Ray Alez <raymestalez@gmail.com>
2017-04-23 23:34:31 -0700

    Update.

--- autobooster.py
+++ autobooster.py
@@ -3,31 +3,35 @@ from mastodon import Mastodon
 import json
 
 def mastodon_bot(botname, email, password, instance_url='https://mastodon.social'):
+    # This will be used to generate unique names for files containing your credentials
+    # So that you could run several bots with different credentials
+    account_name = email+"_"+instance_url.replace("https://","")+"_"
+
     # Create app if doesn't exist
-    if not os.path.isfile("clientcred.txt"):
+    if not os.path.isfile(account_name + "clientcred.txt"):
         print("Creating app")
         mastodon = Mastodon.create_app(
             botname,
-            to_file = 'clientcred.txt',
+            to_file = account_name + 'clientcred.txt',
             api_base_url=instance_url
         )
     
     # Fetch access token if I didn't already
-    if not os.path.isfile("usercred.txt"):
+    if not os.path.isfile(account_name + "usercred.txt"):
         print("Logging in")
         mastodon = Mastodon(
-            client_id = 'clientcred.txt',
+            client_id = account_name + 'clientcred.txt',
             api_base_url=instance_url        
         )
-        mastodon.log_in(email, password, to_file = 'usercred.txt')
+        mastodon.log_in(email, password, to_file = account_name + 'usercred.txt')
     
     # Login using generated auth
     mastodon = Mastodon(
-        client_id = 'clientcred.txt',
-        access_token = 'usercred.txt',
+        client_id = account_name + 'clientcred.txt',
+        access_token = account_name + 'usercred.txt',
         api_base_url=instance_url    
     )
-    print(botname + " logged into " + instance_url)
+    print(botname + " logged in as " + email + " at " + instance_url)
 
     return mastodon
 
@@ -39,11 +43,12 @@ def repost_recent_statuses(mastodon, source_username):
     source_account = mastodon._Mastodon__api_request('POST',
                                                      '/api/v1/follows',
                                                      params={"uri":source_username})
+    print("source_account " + str(source_account))
     recent_statuses = mastodon.account_statuses(source_account["id"])
     print("Fetched recent statuses from " + source_username)
     # print(recent_statuses)
     
-    for status in recent_statuses[:2]:
+    for status in recent_statuses[:10]:
         try:
             mastodon.status_reblog(status["id"])
             print("Reblogged status " + str(status["id"]))
@@ -55,10 +60,18 @@ with open('config.json', 'r') as f:
     config = json.load(f)
     
 
-botname = config["botname"]
-email = config["email"]
-password = config["password"]
-instance_url = config["instance_url"]
+
+try:
+    # Can also pass the rebooster account credentials as arguments
+    botname =  sys.argv[2]
+    email =  sys.argv[3]
+    password =  sys.argv[4]
+    instance_url =  sys.argv[5]
+except:
+    botname = config["botname"]
+    email = config["email"]
+    password = config["password"]
+    instance_url = config["instance_url"]
 
 # Log into mastodon
 mastodon = mastodon_bot(botname, email, password, instance_url)
